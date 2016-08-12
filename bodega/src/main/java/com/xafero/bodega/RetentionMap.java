7a5f568681bd8bd9182fc9ae3cc2d0ad32932129
xafero <georg.berendt@uni-oldenburg.de>
2016-08-12 02:16:37 +0200

    Initial code

---
+++ bodega/src/main/java/com/xafero/bodega/RetentionMap.java
@@ -0,0 +1,5 @@
+package com.xafero.bodega;
+
+public class RetentionMap {
+
+}
---
+++ bodega/src/test/java/com/xafero/bodega/RetentionMapTest.java
@@ -0,0 +1,12 @@
+package com.xafero.bodega;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RetentionMapTest {
+
+    @Test
+    public void testMap() {
+    }
+}
